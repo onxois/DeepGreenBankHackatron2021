c2702883e0276c370566e4fc59c21d1d39d8471d
Bernhard Schicht <schicht@stash.de>
2021-01-16 20:27:28 +0100

    small update

--- DeepGreenBankHackatron2021/src/de/stash/bank2021/HelloWorld.java
+++ DeepGreenBankHackatron2021/src/de/stash/bank2021/HelloWorld.java
@@ -3,8 +3,7 @@ package de.stash.bank2021;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-
+		System.out.println("Hello World! Here I am.");
 	}
 
 }
